939ef5bc21f2fa1bfe7b3bdbeed48b2a08f5ee80
Hoang Tran <hoang.tran@apps-cyclone.com>
2024-09-25 23:29:05 +0700

    feat(routes)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
-const express = require("express");
-const router = express.Router();
+const coursesRouter = require("./courses");
+const meRouter = require("./me");
+const newsRouter = require("./news");
+const siteRouter = require("./site");
 function router(app) {
-  app.use("/courses");
-  app.use("/me");
+  app.use("/courses", coursesRouter);
+  app.use("/me", meRouter);
+  app.use("/news", newsRouter);
+  app.use("/", siteRouter);
 }
-module.exports = { router };
+module.exports = router;
