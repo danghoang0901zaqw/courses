8b4d88afd10333842e7d67beafd5f0a117c4e003
Hoang Tran <hoang.tran@apps-cyclone.com>
2024-09-30 23:58:49 +0700

    feat(auto-increament-id)

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const slug = require("mongoose-slug-updater");
 const mongooseDelete = require("mongoose-delete");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const Schema = mongoose.Schema;
 const Course = new Schema(
   {
+    _id: { type: Number },
     name: { type: String, required: true },
     description: { type: String },
     image: { type: String },
@@ -13,11 +15,13 @@ const Course = new Schema(
     slug: { type: String, slug: "name", unique: true },
   },
   {
+    _id: false,
     timestamps: true,
   }
 );
 
 Course.plugin(mongooseDelete, { deletedAt: true, overrideMethods: "all" });
+Course.plugin(AutoIncrement);
 mongoose.plugin(slug);
 
 module.exports = mongoose.model("Course", Course);
